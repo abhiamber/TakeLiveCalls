b8d90c9fc8f50ead3ec710ae53d3c3d2fc750554
abhiamber191 <abhiamber191@gmail.com>
2023-05-14 23:06:53 +0530

    middleware

--- live-frontend/src/component/EventList.js
+++ live-frontend/src/component/EventList.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const EventLists = ({ events }) => {
-  // console.log(events)
+  console.log(events)
   return (
     <>
       <Box
